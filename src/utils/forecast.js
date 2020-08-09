513ebe21859c1caa4fab0b78f63ad07ba28cc272
Steven Kim <97stevenkim@gmail.com>
2020-08-09 15:00:45 -0400

    humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees out. \nIt feels like ${body.current.feelslike} degrees. \nThe chance for rain is ${body.current.precip}%.`);
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees out. \nIt feels like ${body.current.feelslike} degrees. \nThe humidity is ${body.current.humidity}.`);
         }
     })
 }
