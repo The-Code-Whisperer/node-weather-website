d4de693f2a08e3024ba132add867522e732732dc
Steven Kim <97stevenkim@gmail.com>
2020-08-09 15:46:14 -0400

    celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=1b3b1f0fba26d786af7452c839f95291&query=' + latitude + ',' + longitude + '&units=f';
+    const url = 'http://api.weatherstack.com/current?access_key=1b3b1f0fba26d786af7452c839f95291&query=' + latitude + ',' + longitude + '&units=';
 
     request({ url, json: true }, (error, {body}) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees out. \nIt feels like ${body.current.feelslike} degrees. \nThe humidity is ${body.current.humidity}.`);
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature}°C out. It feels like ${body.current.feelslike}°C. The humidity is ${body.current.humidity}%.`);
         }
     })
 }
